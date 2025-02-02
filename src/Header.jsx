4dcd0a2fa2a3b1c8091c602a2d91bcb5ca231985
x1u49jd <jakub.dzido00@gmail.com>
2025-02-02 20:58:32 +0100

    Applied styling to the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header(){
     return (
-        <>
+        <header>
             <img src="./images/reactjs-icon.png"/>
             <p>ReactFacts</p>
-        </>
+        </header>
     )
 }
