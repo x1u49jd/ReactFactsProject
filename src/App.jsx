6aa85727907401783c760e5f42f2b99fa28f37aa
x1u49jd <jakub.dzido00@gmail.com>
2025-02-03 19:37:21 +0100

    Applied styling to the main and replaced the icon image

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Main from "/src/Main";
 
 export default function App(){
     return (
-        <>
+        <div className="app">
             <Header />
             <Main />
-        </>
+        </div>
     )
 }
